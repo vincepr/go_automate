4d646258eb0dd3099f7a2c4945e2f4789eafaf07
vincepr <vinceprinceking@gmail.com>
2023-04-04 13:37:27 +0200

    output formating

--- shedule/shedule.go
+++ shedule/shedule.go
@@ -22,7 +22,7 @@ type InfoChan struct{
 
 func RunOnce(p yaml_parser.ProcessOnce, ch chan InfoChan){
 	dur := p.When.Sub(time.Now())
-	log.Printf("SCHEDULED: %s;	CMD: %s;	IN: %v;",p.Name,p.Cmd,dur)
+	log.Printf("SCHEDULED: %s; IN: %v; CMD: %s",p.Name,dur.Round(time.Second),p.Cmd)
 	time.Sleep(dur)
 	log.Printf("STARTED: %s;	CMD: %s;",p.Name,p.Cmd)
 	out := cmd.Run(p.Cmd)
@@ -39,10 +39,10 @@ func RunRepeating(p yaml_parser.ProcessRepeat, ch chan InfoChan){
 	if p.RepeatTimes == 0 {p.RepeatTimes=1}
 	counter := p.RepeatTimes
 	dur := p.When.Sub(time.Now())
-	log.Printf("SCHEDULED: %s;	CMD: %s;	IN: %v; REPEATS: %v;",p.Name,p.Cmd,dur,counter)
+	log.Printf("SCHEDULED: %s; IN: %v; REPEATS: %v; CMD: %s;",p.Name,dur.Round(time.Second),counter,p.Cmd)
 	time.Sleep(dur)
 	for counter>0{
-		log.Printf("STARTED: %s;		CMD: %s; REPEATS: %v;",p.Name,p.Cmd,counter)
+		log.Printf("STARTED: %s; IN: %v REPEATS: %v; CMD: %s",p.Name,dur.Round(time.Second),counter,p.Cmd)
 		out := cmd.Run(p.Cmd)
 		out = strings.TrimSuffix(out, "\n")
 		ch <- InfoChan{
@@ -70,9 +70,11 @@ func RunRepeating(p yaml_parser.ProcessRepeat, ch chan InfoChan){
 
 // "WaitGroup", takes count of how many process are not finished
 var countPr int32
+
 func ReadWg() int32{
 	return atomic.LoadInt32(&countPr)
 }
+
 func AddWg(change int32){
 	wg := ReadWg()
 	atomic.StoreInt32(&countPr, wg+change)
